db2b6c4dc38c527792febf3a9fa4f6e4cddf0779
zhangly <dear531@21cn.com>
2014-07-28 21:11:52 +0800

    fix multiple ip address

--- snmpgetinfo.sh
+++ snmpgetinfo.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 source	./snmpgetinfo.conf
-sed -n '/^ip*=*\.*\.*\.*/'p snmpgetinfo.conf 
-
+ips=`sed -n '/^ip=*\.*\.*\.*/'p snmpgetinfo.conf |sed -e 's/ip=//' |sort |uniq`
 
 #smartgrid
 snmpwalk=/SmartGrid/snmp/bin/snmpwalk
 #debug
 snmpwalk=snmpwalk
-	get_cpu=`$snmpwalk -v 3 -l authNoPriv -u $usm_user -a $auth_algrorithm -A $auth_password 192.168.12.78 \
-	.1.3.6.1.4.1.99999.16 2>/dev/null`
+for ipsingle in $ips
+do
+	echo $ipsingle;
+done
+	get_cpu=`$snmpwalk -v 3 -l authNoPriv -u $usm_user -a $auth_algrorithm -A $auth_password \
+	$ipsingle .1.3.6.1.4.1.99999.16 2>/dev/null`
 	result=`echo $?`;
 	if [[ $result -ne 0 ]];
 	then
 		echo "result=failure";
-		exit 1;
 	fi
 	cpu_array=`echo "$get_cpu" |awk '{print $7}'`
 	sum=0;
@@ -28,13 +30,12 @@ snmpwalk=snmpwalk
 	done
 	sum=$(echo "scale=2; $sum/$num" | bc -l)
 
-	get_mem=`$snmpwalk -v 3 -l authNoPriv -u $usm_user -a $auth_algrorithm -A $auth_password 192.168.12.78 \
-	.1.3.6.1.4.1.99999.15 2>/dev/null`
+	get_mem=`$snmpwalk -v 3 -l authNoPriv -u $usm_user -a $auth_algrorithm -A $auth_password \
+		$ipsingle .1.3.6.1.4.1.99999.15 2>/dev/null`
 	result=`echo $?`;
 	if [[ $result -ne 0 ]];
 	then
 		echo "result=failure";
-		exit 2;
 	fi
 	echo "result=success";
 #	freemem=`echo "$get_mem" | awk '{if (NR==3)print $7}'`
@@ -46,3 +47,4 @@ snmpwalk=snmpwalk
 	mem_array=`echo "$get_mem" | awk '{if (NR==3)print $7/$5}'`
 	echo -e "cpu: $sum\\nmem: $mem_array";
 #	echo "$get_mem";
+	exit 0;
